210b1ba5ccdd73e7ace3d29acbaf235eb0b19b25
Eric Le Bihan <eric.le.bihan.dev@free.fr>
2015-07-03 22:22:21 +0200

    Bump to next unstable release

--- vestricius/__init__.py
+++ vestricius/__init__.py
@@ -8,15 +8,15 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
-# 
+#
 
 """
    vestricius
@@ -29,6 +29,6 @@
 """
 
 __docformat__ = 'restructuredtext en'
-__version__ = '0.1.1'
+__version__ = '0.1.3'
 
 # vim: ts=4 sw=4 sts=4 et ai
